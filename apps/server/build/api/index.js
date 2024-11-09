6a9b819ed75ad9e596887a22e25242daa226d40e
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2024-11-09 15:14:53 +0000

    Try 1203131

--- apps/server/build/api/index.js
+++
@@ -1,9 +0,0 @@
-import { setupFastify } from "../dist/fastify/index.js";
-
-const app = await setupFastify();
-
-// api/index.js
-export default async function handler(req, reply) {
-  await app.ready();
-  app.server.emit("request", req, reply);
-}
