1a020560a17e298bc45e8a81ee1a85bc3899055d
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2024-11-09 14:21:51 +0000

    Try fix

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -4,6 +4,7 @@ import initFastify, { setupFastify } from "./fastify/index.js";
 
 const app = await setupFastify();
 
+// index.js
 export default async function handler(req: any, reply: any) {
   await app.ready();
   app.server.emit("request", req, reply);
